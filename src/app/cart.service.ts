cb153a44d3829991f59989788fc5566dba4fe90d
Aileander <antsonfire85@gmail.com>
2020-01-24 09:40:49 +0000

    Shopping Cart Service Setup

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -5,6 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class CartService {
 
+  //constructor () {}
   items = [];
 
   addToCart(product) {
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -11,8 +12,14 @@ import { products } from '../products';
 export class ProductDetailsComponent implements OnInit {
   product;
 
+  addToCart(product) {
+    window.alert('Good news you now have an item in your cart!');
+    this.cartService.addToCart(product);
+  }
+
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService
     ) { }
 
   ngOnInit() {
